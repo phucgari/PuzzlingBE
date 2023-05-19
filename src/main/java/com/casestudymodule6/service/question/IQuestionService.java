f1bda51ea010ad392fa941222804653925ee147a
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-05-19 09:01:35 +0700

    update

--- src/main/java/com/casestudymodule6/service/question/IQuestionService.java
+++
@@ -1,7 +0,0 @@
-package com.casestudymodule6.service.question;
-
-import com.casestudymodule6.model.question.Question;
-import com.casestudymodule6.service.IGeneralService;
-
-public interface IQuestionService extends IGeneralService<Question> {
-}
