f7218673c71495d6c0aad456ebff5149df219130
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-05-28 21:35:45 +0700

    update

--- src/main/java/com/casestudymodule6/controller/RecordController.java
+++ src/main/java/com/casestudymodule6/controller/RecordController.java
@@ -2,16 +2,12 @@ package com.casestudymodule6.controller;
 
 import com.casestudymodule6.model.dto.RecordDTO;
 import com.casestudymodule6.model.record.Record;
-import com.casestudymodule6.model.record.RecordDetail;
 import com.casestudymodule6.service.exam.IExamService;
 import com.casestudymodule6.service.record.IRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin("*")
 @RestController
